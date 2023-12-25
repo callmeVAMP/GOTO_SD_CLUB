f2bff4094ae1af738581d1c94a0f11c0710eb1a9
VARSHA SWARAJ <126698394+callmeVAMP@users.noreply.github.com>
2023-12-25 19:23:57 +0530

    Update LoginPage.tsx
    added css

--- deployment/client/src/Pages/LoginPage.tsx
+++ deployment/client/src/Pages/LoginPage.tsx
@@ -1,41 +1,55 @@
+import react , {FunctionComponent} from "react";
 import InfoCard from '../components/InfoCard';
 import "../styles/loginPageStyle.css"
 import loginPageProps from '../types/loginPageProps';
 import {GoogleLogin} from '@react-oauth/google';
-import TitlebarBelowImageList from './Demo';
-//import { StyledEngineProvider } from '@mui/material/styles';
+import { StyledEngineProvider } from '@mui/material/styles';
+//import "../styles/images.css";
+import sass from 'sass';
 
-//import Demo from './Demo';
+import images from "../types/images";
+//import TitlebarBelowImageList from '../types/TitlebarBelowImageList';
+import Carousel from "../types/Carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 const LoginPage : React.FC<loginPageProps> = ({handleLoginSuccess}) => {
     return (
         <>
-            <div>
-                <div id="google-one-tap-button">
-                    <InfoCard content="Hello there, please login to continue"/>
-                    <h1>YOUR NEXT TRIP STARTS HERE</h1>
-                    <GoogleLogin
-                        onSuccess={credentialResponse => {
-                            handleLoginSuccess(credentialResponse);
-                        }}
-                        onError={() => {
-                            console.log('Login Failed');
-                            window.alert('Login Failed. Please retry.');
-                        }}
+            <div className="page1">
+                <div className="firstDiv">
+                    <img id = "first_img" src="https://i.ytimg.com/vi/VVNt3cLjX3c/maxresdefault.jpg" alt="" width="100%" />
+                    <div className="textt"> <h1>Let's Begin The Journey TOGETHER!!!</h1>
+                    
+                    
+                    <div id="google-one-tap-button">
+                    
+                        
+                        <GoogleLogin
+                            onSuccess={credentialResponse => {
+                                handleLoginSuccess(credentialResponse);
+                            }}
+                            onError={() => {
+                                console.log('Login Failed');
+                                window.alert('Login Failed. Please retry.');
+                            }}
 
-                        useOneTap={true}
-                        prompt_parent_id='google-one-tap-button'
-                        cancel_on_tap_outside={false}
-                        theme='filled_black'
-                        size='large'
-                        shape='pill'
-                        width='250px'
-                    />
+                            useOneTap={true}
+                            prompt_parent_id='google-one-tap-button'
+                            cancel_on_tap_outside={false}
+                            theme='filled_black'
+                            size='large'
+                            shape='pill'
+                            width='250px'
+                        />
+                        
+                    </div>
+                    </div>
                 </div>
+                <div className="Topic2">Places you can VISIT</div>
+                <Carousel />
+                
             </div>
+            
         </>
     );
 };
-
-
-
 export default LoginPage;
