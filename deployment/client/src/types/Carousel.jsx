22db58fabe6544f694e5143dc6db9eb89f10d19a
VARSHA SWARAJ <126698394+callmeVAMP@users.noreply.github.com>
2023-12-25 19:31:16 +0530

    Create Carousel.jsx
    added image list

---
+++ deployment/client/src/types/Carousel.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+
+export default () => (
+  <Carousel autoPlay>
+    
+    <div className="containerr">
+      <img className="image" alt="" src="https://i.ytimg.com/vi/hIGrhoxL0mY/maxresdefault.jpg" />
+      <div className="middle">
+        <div className="text">KHURDA ROAD STATION</div>
+      </div>
+      
+    </div>
+    <div className="containerr">
+      <img className="image" alt="" src="https://th.bing.com/th/id/R.ac6c53365e329d771d38993153b3aeac?rik=hvGC%2f%2fB8lRjWcw&riu=http%3a%2f%2ftimesofindia.indiatimes.com%2fphoto%2fmsid-60516973%2f60516973.jpg%3f190170&ehk=Opn6HE3%2fW6m3SZjJpzMUf8ck8PmcHbrClqSORU0unm0%3d&risl=&pid=ImgRaw&r=0" />
+      <div className="middle">
+        <div className="text">BHUBNESHWAR STATION</div>
+      </div>
+      
+    </div>
+    <div className="containerr">
+      <img className="image" alt="" src="https://static-blog.treebo.com/wp-content/uploads/2022/11/Nexus-Esplanade-Bhubaneshwar.jpg" />
+      <div className="middle">
+        <div className="text">NEXUS ESPALANDE</div>
+      </div>
+      
+    </div>
+    <div className="containerr">
+      <img className="image" alt="" src="https://ebhubaneswar.com/wp-content/uploads/2019/05/dn-regalia-orrisa-big.jpg" />
+      <div className="middle">
+        <div className="text">DN REGALIA</div>
+      </div>
+      
+    </div>
+    
+    
+  
+      
+  </Carousel>
+);
